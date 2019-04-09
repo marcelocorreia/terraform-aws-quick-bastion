3ae4793625bef12de451387a8bb93c0cfa3480af
Marcelo Correia <marcelo@correia.io>
2019-04-09 23:36:35 +1000

    WIP://

--- example/main.tf
+++ example/main.tf
@@ -27,13 +27,14 @@ module "bastion" {
   bastion_userdata = "${file("custom_userdata.sh")}"
 }
 
+// Creating a new SSH Key
 module "ssh_key" {
   source     = "git::https://github.com/marcelocorreia/terraform-aws-keypair.git?ref=master"
   key_name   = "bastion"
   key_path   = "/tmp/bastion"
   create_key = true
 }
-// Creatting a new SSH Key
+
 module "bastion2" {
   source           = "../"
   ami_source_owner = "122455764658"
